ed0ebd8e573f7e8010f6e1c1bf26edc6f1fa469f
MAYAUERS <meerapotdar999@gmail.com>
2021-08-05 12:53:22 +0530

    New Rx java Observable types

--- app/src/main/java/com/example/a4augusrrightapiwithrxjava/ApiInterface.java
+++ app/src/main/java/com/example/a4augusrrightapiwithrxjava/ApiInterface.java
@@ -2,13 +2,20 @@ package com.example.a4augusrrightapiwithrxjava;
 
 import com.google.gson.JsonObject;
 
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
 //https://balicuat.bajajallianz.com/INSTAB/ws/api/usright
     @POST("INSTAB/ws/api/usright")
-    Observable<SucessResponse> getAllAPIResponse(@Body JsonObject jsonObject);
+//    Observable<SucessResponse> getAllAPIResponse(@Body JsonObject jsonObject);
+    //Single<SucessResponse> getAllAPIResponse(@Body JsonObject jsonObject);
 
+  //  Flowable<SucessResponse> getAllAPIResponse(@Body JsonObject jsonObject);
+    Maybe<SucessResponse> getAllAPIResponse(@Body JsonObject jsonObject);
 }
